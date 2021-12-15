8397f81957359aecbe9d7b5b4d007be373bd4e41
Jonathan <jonato96s@gmail.com>
2021-12-14 21:27:44 -0500

    Ahora usamos anotaciones

--- cdi01/src/main/java/com/programacion/servicios/OperacionesImpl.java
+++ cdi01/src/main/java/com/programacion/servicios/OperacionesImpl.java
@@ -1,5 +1,8 @@
 package com.programacion.servicios;
 
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class OperacionesImpl implements Operaciones{
     public int suma(int x, int y) {
         return x+y;
