7ad2febfc13e6b38dc1acaee7db5105d0952146e
Jonathan <jonato96s@gmail.com>
2021-12-14 22:24:44 -0500

    Verificar ciclo de vida: Constructor, @PostConstruct, @PreDestroy

--- cdi01/src/main/java/com/programacion/servicios/OperacionesImpl.java
+++ cdi01/src/main/java/com/programacion/servicios/OperacionesImpl.java
@@ -1,5 +1,7 @@
 package com.programacion.servicios;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -7,6 +9,20 @@ import jakarta.inject.Inject;
 public class OperacionesImpl implements Operaciones{
     @Inject
     private Log log;
+
+    public OperacionesImpl(){
+        System.out.println("Estoy en el constructor");
+    }
+
+    @PostConstruct
+    void init(){
+        System.out.println("Ciclo de vida @PostConstruc");
+    }
+    @PreDestroy
+    void destroy(){
+        System.out.println("Ciclo de vida @PreDestroy");
+    }
+
     public int suma(int x, int y) {
         log.log("Inicializando operacion");
         return x+y;
