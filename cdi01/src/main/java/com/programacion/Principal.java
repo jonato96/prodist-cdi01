ea65e428245a262cb6e6c59d8bc9b03b47659ff7
Jonathan <jonato96s@gmail.com>
2021-12-14 21:19:16 -0500

    Proyeto base con lookup, se buscar cualquier posible componente

---
+++ cdi01/src/main/java/com/programacion/Principal.java
@@ -0,0 +1,21 @@
+package com.programacion;
+
+import com.programacion.servicios.Operaciones;
+import jakarta.enterprise.inject.Instance;
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
+
+public class Principal {
+    public static void main(String[] args) {
+        //Inicializamos el contenedor
+        SeContainer container = SeContainerInitializer
+                .newInstance()
+                .initialize();
+        //Buscamos el componente
+        Instance<Operaciones> servicio = container.select(Operaciones.class);
+        //Obtenemos el metodo
+        Operaciones op = servicio.get();
+        int resultado = op.suma(2,2);
+        System.out.println("El resultado de la suma es: "+resultado);
+    }
+}
---
+++ cdi01/src/main/java/com/programacion/servicios/OperacionesImpl.java
@@ -0,0 +1,7 @@
+package com.programacion.servicios;
+
+public class OperacionesImpl implements Operaciones{
+    public int suma(int x, int y) {
+        return x+y;
+    }
+}
